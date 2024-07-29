c477a6ab85c3a8e6ed36527384bff07de19eddd4
King <mac@mac.com>
2024-07-29 12:52:21 +0900

    12-7

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -5,6 +5,8 @@ import Diary from "./pages/Diary.jsx";
 import New from "./pages/New.jsx";
 import Notfound from "./pages/Notfound.jsx";
 import {getEmotionImage} from "./util/get-emotion-image.jsx";
+import Button from "./components/Button.jsx";
+import Header from "./components/Header.jsx";
 
 
 function App() {
@@ -14,29 +16,42 @@ function App() {
         nav("/new");
     }
 
-    return (
-        <>
-            <div>
-                <img src={getEmotionImage(1)} alt="emotion1"/>
-                <img src={getEmotionImage(2)} alt="emotion2"/>
-                <img src={getEmotionImage(3)} alt="emotion3"/>
-                <img src={getEmotionImage(4)} alt="emotion4"/>
-                <img src={getEmotionImage(5)} alt="emotion5"/>
-            </div>
-            <div>
-                <Link to='/'>Home</Link>
-                <Link to='/New'>New</Link>
-                <Link to='/Diary'>Diary</Link>
-            </div>
-            <button onClick={onClick}>go to new page</button>
-            <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/new" element={<New/>}/>
-                <Route path="/diary/:id" element={<Diary/>}/>
-                <Route path="*" element={<Notfound/>}/>
-            </Routes>
+    return (<>
+        <Header
+            title={"Header"}
+            leftChild={<Button text={"Left"}/>}
+            rightChild={<Button text={"Right"}/>}/>
+        <Button text={"123"}
+                onClick={() => {
+                    console.log("123번 버튼 클릭");
+                }}
+        />
+        <Button text={"123"}
+                onClick={() => {
+                    console.log("123번 버튼 클릭");
+                }}
+                type={"NEGATIVE"}
+        />
+        <Button text={"123"}
+                onClick={() => {
+                    console.log("123번 버튼 클릭");
+                }}
+                type={"POSITIVE"}
+        />
+        <div>
+            <Link to='/'>Home</Link>
+            <Link to='/New'>New</Link>
+            <Link to='/Diary'>Diary</Link>
+        </div>
+        <button onClick={onClick}>go to new page</button>
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/new" element={<New/>}/>
+            <Route path="/diary/:id" element={<Diary/>}/>
+            <Route path="*" element={<Notfound/>}/>
+        </Routes>
 
-        </>);
+    </>);
 }
 
 export default App
