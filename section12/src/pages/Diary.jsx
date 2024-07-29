c9eac60a15c615f03f1cc3ca57096cff94a672be
King <mac@mac.com>
2024-07-29 12:11:59 +0900

    12.5

--- section12/src/pages/Diary.jsx
+++ section12/src/pages/Diary.jsx
@@ -1,7 +1,11 @@
+import {useParams} from "react-router-dom";
+
 const Diary = () => {
+
+    const params = useParams();
     return (
         <div>
-            <h1>Diary</h1>
+            <h1>{params.id}번 일기 입니다.</h1>
         </div>
     );
 }
