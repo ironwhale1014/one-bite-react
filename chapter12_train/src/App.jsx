5be5a52ccf316461b4ace04cd7a0896c843b14b1
King <mac@mac.com>
2024-08-05 08:22:49 +0900

    initial

--- chapter12_train/src/App.jsx
+++ chapter12_train/src/App.jsx
@@ -1,8 +1,12 @@
 import './App.css'
+import {Route, Routes} from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Diary from "./pages/Diary.jsx";
 
 function App() {
 
     return (<>
+
     </>);
 }
 
