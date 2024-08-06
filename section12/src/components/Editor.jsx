6c198b07aa6918abe5e4fab7acfd914c9bfe95a2
King <mac@mac.com>
2024-08-06 12:41:44 +0900

    12.14

--- section12/src/components/Editor.jsx
+++ section12/src/components/Editor.jsx
@@ -1,6 +1,8 @@
 import "./Editor.css";
 import Button from "./Button.jsx";
 import EmotionItem from "./EmotionItem.jsx";
+import {useState} from "react";
+import {useNavigate} from "react-router-dom";
 
 const emotionData = [
     {
@@ -25,30 +27,75 @@ const emotionData = [
     },
 ]
 
-const Editor = () => {
+const Editor = ({onSumbit}) => {
 
-    const emotionId = 2 ;
 
+    const dateToString = (date) => {
+        let year = date.getFullYear();
+        let month = date.getMonth() + 1;
+        let day = date.getDate();
+
+        if (month < 10) {
+            month = "0" + month;
+        }
+        if (day < 10) {
+            day = "0" + day;
+        }
+
+        return `${year}-${month}-${day}`;
+    }
+
+    const onChangeInput = (e) => {
+
+        let name = e.target.name;
+        let value = e.target.value;
+        console.log(name, value);
+        if (name === "createDate") {
+            value = new Date(value);
+        }
+        setInput({
+            ...input,
+            [name]: value
+        })
+    }
+
+    const [input, setInput] = useState({
+        createDate: new Date(),
+        emotionId: 3,
+        content: ""
+    });
+
+
+    const nav = useNavigate();
+    const onSubmitButtonClick = () => {
+        onSumbit(input);
+    }
     return (
         <div className="editor">
             <section className="date_section">
                 <h4>오늘의 날짜</h4>
-                <input type={"date"}/>
+                <input name="createDate" type={"date"} value={dateToString(input.createDate)} onChange={onChangeInput}/>
             </section>
             <section className="emotion_section">
                 <h4>오늘의 감정</h4>
                 <div className="emotion_list_wrapper">
                     {emotionData.map((item) =>
-                        <EmotionItem key={item.id} {...item} isSelected={emotionId === item.emotionId}/>)}
+                        <EmotionItem
+                            onClick={() => onChangeInput({target: {name: "emotionId", value: item.emotionId}})}
+                            key={item.id} {...item} isSelected={item.emotionId === input.emotionId}/>)}
                 </div>
             </section>
             <section className="content_section">
                 <h4>오늘의 일기</h4>
-                <textarea type={"text"} placeholder="오늘은 어땠나요?"/>
+                <textarea
+                    name="content"
+                    value={input.content}
+                    onChange={onChangeInput}
+                    type={"text"} placeholder="오늘은 어땠나요?"/>
             </section>
             <section className="button_section">
-                <Button text={"취소 하기"}/>
-                <Button text={"작성 완료"} type={"POSITIVE"}/>
+                <Button onClick={()=>nav(-1)} text={"취소 하기"}/>
+                <Button onClick={onSubmitButtonClick} text={"작성 완료"} type={"POSITIVE"}/>
             </section>
 
 
--- section12/src/pages/New.jsx
+++ section12/src/pages/New.jsx
@@ -1,16 +1,30 @@
 import Header from "../components/Header.jsx";
 import Button from "../components/Button.jsx";
 import Editor from "../components/Editor.jsx";
+import {useNavigate} from "react-router-dom";
+import {useContext} from "react";
+import {DiaryDispatchContext} from "../App.jsx";
 
 const New = () => {
+
+    const nav = useNavigate();
+    const {onCreate} = useContext(DiaryDispatchContext)
+
+    const onSubmit = (input) => {
+        onCreate(input.createDate.getTime(), input.emotionId, input.content);
+        nav("/", {replace: true});
+    }
+
     return (
         <div className="new">
             <div className="header">
                 <Header title={"새 일기 쓰기"}
-                        leftChild={<Button text={"< 뒤로 가기"}/>}/>
+                        leftChild={<Button text={"< 뒤로 가기"} onClick={() => {
+                            nav(-1)
+                        }}/>}/>
             </div>
             <div className="Editor">
-                <Editor/>
+                <Editor onSumbit={onSubmit}/>
             </div>
         </div>
     );
